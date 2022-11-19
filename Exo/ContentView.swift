e5682442dc306eb7515f9ef2dc129602b5163f5b
AtikaHar <haryouli.atika@gmail.com>
2022-11-19 18:48:38 +0100

    Initial Commit

---
+++ Exo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exo
+//
+//  Created by Atika Haryouli on 19/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exo/ExoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoApp.swift
+//  Exo
+//
+//  Created by Atika Haryouli on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
